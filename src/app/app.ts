03f0f63b25beff1c6aa06dda8956d4b476212d4a
Kayki Letieri <kaykiletieri37@gmail.com>
2026-03-22 16:22:20 -0300

    feat: add footer component with template, styles, and tests

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,10 +6,11 @@ import { HowIHelp } from './features/home/how-i-help/how-i-help';
 import { Approach } from './features/home/approach/approach';
 import { HowItWorks } from './features/home/how-it-works/how-it-works';
 import { OnlineService } from './features/home/online-service/online-service';
+import { Footer } from './shared/components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Hero, About, HowIHelp, Approach, HowItWorks, OnlineService],
+  imports: [Navbar, Hero, About, HowIHelp, Approach, HowItWorks, OnlineService, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/shared/components/footer/footer.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { LucideAngularModule, Phone, Mail, Instagram } from 'lucide-angular';
+import { ScrollAnimateDirective } from '../../../core/directives/scroll-animate.directive';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [ScrollAnimateDirective, LucideAngularModule],
+  templateUrl: './footer.html',
+  styleUrl: './footer.scss',
+})
+export class Footer {
+  readonly Phone = Phone;
+  readonly Mail = Mail;
+  readonly Instagram = Instagram;
+  readonly email = 'psi.anaalmeiida14@gmail.com';
+  readonly instagramUsername = 'psi.anaalmeiida';
+  readonly phoneNumber = '(35) 99194-9357';
+
+  readonly currentYear = new Date().getFullYear();
+
+  readonly contact = {
+    phone: this.phoneNumber,
+    phoneHref: `tel:+55${this.phoneNumber.replace(/\D/g, '')}`,
+    email: this.email,
+    emailHref: `mailto:${this.email}`,
+    instagram: `@${this.instagramUsername}`,
+    instagramHref: `https://www.instagram.com/${this.instagramUsername}/`
+  };
+}
