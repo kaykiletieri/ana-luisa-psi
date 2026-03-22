ef703007f7c82f07bad2aac9178ca348df3a1ede
Kayki Letieri <kaykiletieri37@gmail.com>
2026-03-22 15:25:34 -0300

    feat: add about section with content and styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Navbar } from './shared/components/navbar/navbar';
 import { Hero } from './features/home/hero/hero';
+import { About } from './features/home/about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Hero],
+  imports: [Navbar, Hero, About],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
