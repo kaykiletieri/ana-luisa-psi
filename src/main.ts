1e99c651b5f1317abde56790452cbb02c398e384
Kayki Letieri <kaykiletieri37@gmail.com>
2026-03-22 18:49:54 -0300

    feat: add images

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,14 @@ import { App } from './app/app';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+const styles = {
+  title: 'font-size: 16px; font-weight: bold; color: #6B0F1A;',
+  text: 'font-size: 12px; color: #7A3E1D;',
+  link: 'font-size: 12px; color: #6B0F1A; text-decoration: underline;'
+};
+
+console.log('%c Desenvolvido por Kayki Letieri ', styles.title);
+console.log('%c 📧 letieri.dev@gmail.com', styles.text);
+console.log('%c 💼 https://www.linkedin.com/in/kaykiletieri/', styles.link);
+console.log('%c 🐙 https://github.com/users/kaykiletieri', styles.link);
